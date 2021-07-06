5dc82ff8231c41f0a319b64d9c75697be465fd59
Gyanendro Kh <16163526+GyanendroKh@users.noreply.github.com>
2021-07-06 15:06:54 +0530

    Adding the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useCallback, useEffect, useMemo, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+let gameState = false;
+
+const App: FC = () => {
+  const [playing, setPlaying] = useState(false);
+  const [generation, setGeneration] = useState(0);
+  const DIMS = useMemo(
+    () => ({
+      WIDTH: 25,
+      HEIGHT: 20,
+    }),
+    []
   );
-}
+  const [board, setBoard] = useState<number[][]>(() => {
+    return new Array(DIMS.HEIGHT).fill(new Array(DIMS.WIDTH).fill(0));
+  });
+
+  const generate = useCallback(() => {
+    const randomPoints = new Map<number, number>();
+
+    for (let i = 0; i < 100; i++) {
+      const h = Math.floor(Math.random() * DIMS.HEIGHT);
+      const w = Math.floor(Math.random() * DIMS.WIDTH);
+
+      randomPoints.set(h, w);
+    }
+
+    setBoard((curr) => {
+      return curr.map((r, idx) => {
+        return r.map((_, idxx) => {
+          if (randomPoints.has(idx)) {
+            if (randomPoints.get(idx) === idxx) {
+              return 1;
+            }
+          }
+
+          return 0;
+        });
+      });
+    });
+
+    setGeneration((g) => g + 1);
+  }, [DIMS]);
+
+  useEffect(() => {
+    let previousTime = 0.0;
+    const loop = (time: number) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const deltaTime = time - previousTime;
+      previousTime = time;
+
+      generate();
+
+      if (gameState) {
+        requestAnimationFrame(loop);
+      }
+    };
+
+    if (playing) {
+      requestAnimationFrame(loop);
+    }
+  }, [generate, playing]);
+
+  useEffect(() => {
+    gameState = playing;
+  }, [playing]);
+
+  return (
+    <>
+      {board.map((r, idx) => {
+        return (
+          <div className="row" key={idx}>
+            {r.map((n, idxx) => {
+              return (
+                <div
+                  key={`${idx}-${idxx}`}
+                  className={`cell ${n === 1 ? "black" : "white"}`}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+      <br />
+      <div>Generation: {generation}</div>
+      <br />
+      <button onClick={() => setPlaying((curr) => !curr)}>
+        {playing ? "Pause" : "Play"}
+      </button>
+    </>
+  );
+};
 
 export default App;
